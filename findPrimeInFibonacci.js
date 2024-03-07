472a1e7f5285e91d812b83a3b00243438b3013cf
Berke <berke.sutcu@momentup.com>
2024-03-07 15:11:21 +0300

    Add tests

---
+++ findPrimeInFibonacci.js
@@ -0,0 +1,6 @@
+function findPrimeInFibonacci(n) {
+  // Kodunuzu buraya yazınız
+  return "Hello world!";
+}
+
+module.exports = findPrimeInFibonacci;
---
+++ findPrimeInFibonacci.test.js
@@ -0,0 +1,29 @@
+const findPrimeInFibonacci = require("./findPrimeInFibonacci");
+
+test("should output 0", () => {
+  expect(findPrimeInFibonacci(0)).toBe(0);
+});
+
+test("should output []", () => {
+  expect(findPrimeInFibonacci(1)).toStrictEqual([]);
+});
+
+test("should output [2]", () => {
+  expect(findPrimeInFibonacci(3)).toStrictEqual([2]);
+});
+
+test("should output [2, 3]", () => {
+  expect(findPrimeInFibonacci(5)).toStrictEqual([2, 3]);
+});
+
+test("should output [2, 3, 5, 13]", () => {
+  expect(findPrimeInFibonacci(10)).toStrictEqual([2, 3, 5, 13]);
+});
+
+test("should output Pozitif tam sayı giriniz", () => {
+  expect(findPrimeInFibonacci(-6)).toBe("Pozitif tam sayı giriniz");
+});
+
+test("should output Pozitif tam sayı giriniz", () => {
+  expect(findPrimeInFibonacci("abc")).toBe("Pozitif tam sayı giriniz");
+});
